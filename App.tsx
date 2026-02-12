cfcc49e247cb79353346ad75448695d2e826b291
gourav6746-ai <gourav6746@gmail.com>
2026-02-12 09:57:04 +0545

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,23 @@
+import { Navbar } from "./components/Navbar";
+import { HeroSection } from "./components/HeroSection";
+import { LiveTournaments } from "./components/LiveTournaments";
+import { GamesSection } from "./components/GamesSection";
+import { Leaderboard } from "./components/Leaderboard";
+import { HowItWorks } from "./components/HowItWorks";
+import { CTASection } from "./components/CTASection";
+import { Footer } from "./components/Footer";
+
+export function App() {
+  return (
+    <div className="min-h-screen bg-dark-bg text-white antialiased">
+      <Navbar />
+      <HeroSection />
+      <LiveTournaments />
+      <GamesSection />
+      <Leaderboard />
+      <HowItWorks />
+      <CTASection />
+      <Footer />
+    </div>
+  );
+}
